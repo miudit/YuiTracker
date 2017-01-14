fd3db308a4184e865a6f0bc28ee9e3f8b84f83fa
miudit <daichi.miura@retty.me>
2017-01-14 16:18:55 +0900

    fix

--- php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $db['host'] = 'localhost';
-    $db['dbname'] = 'yuiapp';
-    $db['user'] = 'root';
-    $db['password'] = 'miu1001d';
-    $slackApiKey = 'xoxp-7972272885-7972577940-126867287344-df89acecdde9bfd21eaa2a73cde53a77';
- ?>
